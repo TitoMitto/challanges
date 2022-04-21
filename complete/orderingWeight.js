c2cad721ce6b2e83f3b952f9549ea430440b360f
Tito Mitto <mordeccai9@gmail.com>
2022-04-21 15:49:38 +0300

    first commit

---
+++ complete/orderingWeight.js
@@ -0,0 +1,16 @@
+function orderWeight(strng) {
+    let weightMap = {}
+    strng.split(' ').forEach((item)=>{
+        let total = item.split("").reduce((total, curr)=> total + Number(curr), 0)
+        if(weightMap[total]){
+            weightMap[total].push(item)
+        }else{
+            weightMap[total] = [item]
+        }
+        weightMap[total].sort()
+    })
+    return [...Object.entries(weightMap).map((a)=> a[1].join(' '))].join(' ')
+}
+
+console.log(orderWeight("56 65 74 100 99 68 86 180 90"))
+
