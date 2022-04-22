486ba5863148de443a48db0fe79295a6c26ae919
Tito Mitto <mordeccai9@gmail.com>
2022-04-22 12:37:02 +0300

    solve for x

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
 function valueOfX(eq) {
-    let net = eq.split("=").map((item)=> item.trim())
-    if(net[1].includes("x")) net = net.reverse()
+    let [left, right] = eq.split("=")
+    if(right.includes("x"))[left, right] = [right, left]
     
-    if(net[0].match(/(\-|\+\s)?x/g)) {
-        let x  = net[0].match(/((\-|\+)\s)*x/g)
-        console.log(x)
-        net[0] = net[0].replace(/((\-|\+)\s)*x/g,"").trim()
-        net[0] = net[0].replace(/\-/g, "s").replace(/\+/g, "a")
-        net[0] = net[0].replace(/a/g, "-").replace(/s/g, "+").replace(/^(\d+)/g, "- $1")
-        net[1] = `${net[1]} ${net[0]}` 
-        net[0] = x[0].replace("+ ", "");
-        if(net[0] == "- x"){
-            net[0] = "x"
-            net[1] = net[1].replace(/\-/g, "s").replace(/\+/g, "a")
-            net[1] = net[1].replace(/a/g, "-").replace(/s/g, "+").replace(/^(\d+)/g, "- $1") 
-        }
-    }
-
-    return eval(net[1])//eval(net[1])
-}
+    return (left.includes("- x")? - 1: 1) * eval(eval(right) - eval(left.replace("x", 0)))
+  }
 
 console.log(valueOfX('x - 9 + 9 = 9 - 2'))
 
