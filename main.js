d00c45ed713cd7f96dc77ca8fd236c2dbb78911f
Tito Mitto <mordeccai9@gmail.com>
2022-04-22 10:47:36 +0300

    solve for x

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 function valueOfX(eq) {
-    let net = eq.split("=").map((a) => a.trim())
-    let simplified = '';
-    net.forEach((v, i) => {
-        if (/(\-|\+\s)?x(\s\-|\+)?/g.test(v)) {
-            let symbol = v.match(/(\+|\-)/g);
-            if (/(\+|\-)\s\d+/g.test(v)) {
-                let numb = v.match(/\s(\d+)/g)
-                if (i == 0) net[1] = `${net[1]} ${symbol == "+" ? "-" : "+"} ${numb}`
-                else net[0] = `${net[0]} ${symbol == "+" ? "-" : "+"} ${numb}`;
-                if (i == 0) net[0] = net[0].replace(/\s(\+|\-)\s\d+/g, "")
-                else net[1] = net[1].replace(/\s(\+|\-)\s\d+/g, "");
-            } else if (/(\+|\-)\sx/g.test(v)) {
-                let numb = v.match(/(\d+)\s/g)
-                if (numb != null) {
-                    net[i] = net[i].replace(/(\d+)\s/g, "")
-                    if (i == 0) net[1] = `${net[1]} - ${numb}`
-                    else net[0] = `${net[0]} - ${numb}`;
-                }
-            }
+    let net = eq.split("=").map((item)=> item.trim())
+    if(net[1].includes("x")) net = net.reverse()
+    
+    if(net[0].match(/(\-|\+\s)?x/g)) {
+        let x  = net[0].match(/((\-|\+)\s)*x/g)
+        console.log(x)
+        net[0] = net[0].replace(/((\-|\+)\s)*x/g,"").trim()
+        net[0] = net[0].replace(/\-/g, "s").replace(/\+/g, "a")
+        net[0] = net[0].replace(/a/g, "-").replace(/s/g, "+").replace(/^(\d+)/g, "- $1")
+        net[1] = `${net[1]} ${net[0]}` 
+        net[0] = x[0].replace("+ ", "");
+        if(net[0] == "- x"){
+            net[0] = "x"
+            net[1] = net[1].replace(/\-/g, "s").replace(/\+/g, "a")
+            net[1] = net[1].replace(/a/g, "-").replace(/s/g, "+").replace(/^(\d+)/g, "- $1") 
         }
-    })
-    return net
+    }
+
+    return eval(net[1])//eval(net[1])
 }
 
-console.log(valueOfX('- x = 9 - 2'))
+console.log(valueOfX('x - 9 + 9 = 9 - 2'))
+
+console.log(valueOfX('9 - 3 + x = 8 + 7'))
+
+console.log(valueOfX('9 - x + 5 = 8 + 7'))
 
 // x + 1 = 9 - 2
 // x + 1 // 9 - 2
