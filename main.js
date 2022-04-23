aa9e6823f5d35a6825b54a147adb578cef8b51b8
Tito Mitto <mordeccai9@gmail.com>
2022-04-23 12:23:41 +0300

    move zeros

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
-function valueOfX(eq) {
-    let [left, right] = eq.split("=")
-    if(right.includes("x"))[left, right] = [right, left]
-    
-    return (left.includes("- x")? - 1: 1) * eval(eval(right) - eval(left.replace("x", 0)))
-  }
+function moveZeros(arr) {
+    let zeros = []
+    return [...arr.filter((any)=> any === 0? zeros.length == zeros.push(zero):true ), ...zeros]
+}
 
-console.log(valueOfX('x - 9 + 9 = 9 - 2'))
-
-console.log(valueOfX('9 - 3 + x = 8 + 7'))
-
-console.log(valueOfX('9 - x + 5 = 8 + 7'))
-
-// x + 1 = 9 - 2
-// x + 1 // 9 - 2
-// x = 9 - 2 - 1
-// x = 
+console.log(moveZeros)
